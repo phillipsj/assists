c7ace688a22e99d8df86047e104905eb2413431c
Jamie Phillips <cphill11@gmail.com>
2024-10-20 14:14:58 -0400

    Getting the unit tests wrapped up.

--- tests/tools/test_terraform.py
+++ tests/tools/test_terraform.py
@@ -1,14 +1,17 @@
 from io import BytesIO
+from pathlib import Path
+from string import Template
 from unittest.mock import patch
 from urllib import request
 
+from pyfakefs.fake_filesystem import FakeFilesystem
 from semver import Version
 
 from assists.tools import TerraformTool
 
 
 def test_get_terraform_releases():
-    data = get_test_html()
+    data = get_release_html()
     version_to_find = Version(1, 9, 3)
     with patch.object(request, "urlopen", return_value=data):
         versions = TerraformTool.get_terraform_releases()
@@ -16,7 +19,31 @@ def test_get_terraform_releases():
         assert version_to_find in versions
 
 
-def get_test_html() -> BytesIO:
+def test_get_terraform_version(fs):
+    tf_path = create_terraform_tf(fs, "1.9.3")
+    expected_version = "1.9.3"
+    actual_version = TerraformTool.get_terraform_version(tf_path)
+    assert actual_version == expected_version
+
+
+def test_get_terraform_version_wrong(fs):
+    tf_path = create_terraform_tf(fs, "1.9.3")
+    expected_version = "1.9.2"
+    actual_version = TerraformTool.get_terraform_version(tf_path)
+    assert actual_version != expected_version
+
+
+def create_terraform_tf(fs: FakeFilesystem, version) -> Path:
+    path = Path("/src/terraform.tf")
+    terraform_tf = Template("""terraform {
+                          required_version = "$version"
+                      }
+                   """)
+    fs.create_file(path, contents=terraform_tf.substitute(version=version))
+    return path
+
+
+def get_release_html() -> BytesIO:
     html = """<!DOCTYPE html>
             <html lang="us-en">
                 <head>
